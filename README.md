c25f1145f7f89c51560d0fb4c63392e13c9d5217
makano <makanobush@gmail.com>
2023-11-11 00:27:17 +0300

    readme

---
+++ README.md
@@ -0,0 +1,60 @@
+# rayous-daisyui
+
+**rayous-daisyui** is a component loader utility for rayous. It allows you to easily fetch and add components from the rayous-daisyui GitHub repository into your project. It's a handy tool for extending your rayous app with external [daisyui](https://daisyui.com/) components and managing your project's styling.
+
+## Table of Contents
+
+- [Usage](#usage)
+- [Add Components](#add-components)
+- [Remove Components](#remove-components)
+- [Generate Styles](#generate-styles)
+- [Add DaisyUI](#add-daisyui)
+- [Components](#components)
+
+## Usage
+
+You can use `rayous-daisyui` with `npx` without installing it globally. Here's how you can use the available commands:
+
+### Add Components
+
+To add components from a GitHub repository, use the `add` command. This will fetch the specified components and add them to your project's `components/widget.ts` file.
+
+```bash
+npx rayous-daisyui add button card ...
+``` 
+
+### Remove Components
+
+To remove components from your project, use the `remove` command. This will remove the specified components from your project's `components/widget.ts` file.
+
+```bash
+npx rayous-daisyui remove button card ...
+``` 
+
+### Generate Styles
+
+You can generate the styles for your project using the `styles` command. This will create a `styles/daisy.tail.css` file with the basic Tailwind CSS configuration.
+
+```bash
+npx rayous-daisyui styles
+``` 
+
+### Add DaisyUI
+
+To add DaisyUI to your Tailwind CSS configuration, use the `create` command. This will add DaisyUI to your `tailwind.config.js` file.
+
+```bash
+npx rayous-daisyui create
+```
+
+### Components
+
+You can find available components in the [GitHub repository](https://github.com/kevinJ045/rayous-daisyui/tree/main/components). Choose from the following components:
+-  [accordion.ts](https://github.com/kevinJ045/rayous-daisyui/blob/main/components/accordion.ts)
+-  [badge.ts](https://github.com/kevinJ045/rayous-daisyui/blob/main/components/badge.ts)
+-  [button.ts](https://github.com/kevinJ045/rayous-daisyui/blob/main/components/button.ts)
+-  [card.ts](https://github.com/kevinJ045/rayous-daisyui/blob/main/components/card.ts)
+-  [dropdown.ts](https://github.com/kevinJ045/rayous-daisyui/blob/main/components/dropdown.ts)
+-  [loading.ts](https://github.com/kevinJ045/rayous-daisyui/blob/main/components/loading.ts)
+-  [modal.ts](https://github.com/kevinJ045/rayous-daisyui/blob/main/components/modal.ts)
+-	[progress.ts](https://github.com/kevinJ045/rayous-daisyui/blob/main/components/progress.ts)
---
+++ bin/rayous-daisyui.js
@@ -0,0 +1,105 @@
+const { Command } = require('commander');
+const fs = require('fs');
+
+const program = new Command();
+
+program
+  .command('add [widgets...]')
+  .description('Add widgets to components/widget.ts')
+  .action((widgets) => {
+    // Implement the add function here
+    for (const widget of widgets) {
+      const widgetPath = `components/${widget}.ts`;
+      const sourceURL = `https://github.com/kevinJ045/rayous-daisyui/raw/main/components/${widget}.ts`;
+
+      // Check if the 'components' directory exists, create if not
+      if (!fs.existsSync('components')) {
+        fs.mkdirSync('components');
+      }
+
+      // Fetch and write the widget content
+      const content = fs.readFileSync(sourceURL, 'utf8');
+      fs.writeFileSync(widgetPath, content);
+
+      console.log(`Added ${widget} to components/widget.ts`);
+    }
+  });
+
+program
+  .command('remove [widgets...]')
+  .description('Remove widgets from components/widget.ts')
+  .action((widgets) => {
+    // Implement the remove function here
+    const widgetFilePath = 'components/widget.ts';
+
+    if (!fs.existsSync(widgetFilePath)) {
+      console.log('components/widget.ts does not exist. No widgets to remove.');
+      return;
+    }
+
+    for (const widget of widgets) {
+      const widgetPattern = `import { ${widget}.*`;
+
+      // Read the existing content
+      let content = fs.readFileSync(widgetFilePath, 'utf8');
+
+      // Remove the widget import statement
+      content = content.replace(new RegExp(widgetPattern, 'g'), '');
+
+      // Write the updated content back
+      fs.writeFileSync(widgetFilePath, content);
+
+      console.log(`Removed ${widget} from components/widget.ts`);
+    }
+  });
+
+program
+  .command('styles')
+  .description('Generate styles/daisy.tail.css')
+  .action(() => {
+    // Implement the styles function here
+    const tailwindCSS = `
+@tailwind base;
+@tailwind components;
+@tailwind variants;
+@tailwind utilities;
+`;
+
+    const stylesPath = 'styles/daisy.tail.css';
+
+    if (!fs.existsSync('styles')) {
+      fs.mkdirSync('styles');
+    }
+
+    fs.writeFileSync(stylesPath, tailwindCSS);
+
+    console.log('Generated daisy.tail.css in styles/ directory.');
+  });
+
+program
+  .command('create')
+  .description('Add daisyui to tailwind.config.js')
+  .action(() => {
+    // Implement the create function here
+    const tailwindConfigPath = 'tailwind.config.js';
+
+    if (fs.existsSync(tailwindConfigPath)) {
+      let content = fs.readFileSync(tailwindConfigPath, 'utf8');
+
+      if (!content.includes('daisyui')) {
+        content = content.replace(
+          'plugins: [',
+          `plugins: [\n    require('daisyui'),`
+        );
+
+        fs.writeFileSync(tailwindConfigPath, content);
+        console.log('Added daisyui to tailwind.config.js plugins.');
+      } else {
+        console.log('daisyui is already added to tailwind.config.js plugins.');
+      }
+    } else {
+      console.log('tailwind.config.js does not exist. Please create it.');
+    }
+  });
+
+program.parse(process.argv);
